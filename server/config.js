630eb769978b05cd6f21764f77912ce0dc6a66f6
Froxayn123 <rizkymyusuf123@gmail.com>
2024-03-04 14:30:14 +0700

    feat: connect to MongoDB

--- server/config.js
+++ server/config.js
@@ -1,1 +1,3 @@
 export const PORT = 4444;
+
+export const mongoDBURL = "mongodb+srv://root:root32bookstore@bookstore.a8tfoms.mongodb.net/books-collection?retryWrites=true&w=majority&appName=bookstore";
--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`App is listening to port ${PORT}`);
-});
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
