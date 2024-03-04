b35c4781d9e91fb23e6ae11c193f32eeb8b135bd
Froxayn123 <rizkymyusuf123@gmail.com>
2024-03-04 14:46:10 +0700

    feat: Save a book with mongoose

--- server/index.js
+++ server/index.js
@@ -1,8 +1,31 @@
-import express from "express";
+import express, { response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
+
+//Route for save a Book
+app.post("/books", async (req, res) => {
+  try {
+    if (!request.body.title || !request.body.author || !request.body.publishYear) {
+      return response.status(400).send({
+        message: "Send all required fields: title, author, publishYear",
+      });
+    }
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+    return response.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
 
 mongoose
   .connect(mongoDBURL)
