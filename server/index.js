82d5c25009a0480d49076466fe5bb97c9e8de086
Froxayn123 <rizkymyusuf123@gmail.com>
2024-03-05 09:25:16 +0700

    feat: Adding Route for getting all books from db

--- server/index.js
+++ server/index.js
@@ -28,9 +28,23 @@ app.post("/books", async (req, res) => {
 
     const book = await Book.create(newBook);
     return res.status(201).send(book);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).send({ message: error.message });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send({ message: err.message });
+  }
+});
+
+//Route for getting all books from database
+app.get("/books", async (req, res) => {
+  try {
+    const books = await Book.find({});
+    return res.status(200).json({
+      count: books.length,
+      data: books,
+    });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send({ message: err.message });
   }
 });
 
