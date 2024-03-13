5650a8540e47b4e5bf0354c7bf0807fc8b628d08
Froxayn123 <rizkymyusuf123@gmail.com>
2024-03-13 17:20:16 +0700

    feat: Adding add book card (client-side)

--- client/src/components/BooksCard.jsx
+++ client/src/components/BooksCard.jsx
@@ -1,12 +1,13 @@
 import BookCard from "./BookCard";
 import PropTypes from "prop-types";
 
-const BooksCard = ({ books }) => {
+const BooksCard = ({ books, children }) => {
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
       {books.map((book) => (
         <BookCard key={book._id} book={book} />
       ))}
+      {children}
     </div>
   );
 };
@@ -16,4 +17,5 @@ export default BooksCard;
 
 BooksCard.propTypes = {
   books: PropTypes.array,
+  children: PropTypes.object,
 };
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -33,14 +33,25 @@ const Home = () => {
           </Link>
         </Navbar>
       </header>
-      <main>
+      <main className="md:h-screen lg:h-screen">
         <div className="mt-10 flex flex-col justify-center items-center space-y-4">
           <div className="flex justify-center items-center space-x-3">
             <input type="text" className="w-48" />
             <select></select>
             <select></select>
           </div>
-          {loading ? <Spinner /> : <BooksCard books={books}/>}
+          {loading ? (
+            <Spinner />
+          ) : (
+            <BooksCard books={books}>
+              <Link to="/books/create">
+                <div className="w-[350px] p-8 m-4 flex flex-col justify-center items-center space-y-4 border-2 rounded-lg relative hover:shadow-xl transition-all ease-in-out duration-100 border-blue-600">
+                  <MdOutlineAddBox className="text-8xl text-blue-600" />
+                  <h1 className="font-madimiOne text-4xl text-blue-600">Add Book</h1>
+                </div>
+              </Link>
+            </BooksCard>
+          )}
         </div>
       </main>
       <footer>
